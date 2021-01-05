7d263238e34e56af0c32afbe2b21773c160a3829
Galit <galit.gerasimov3@gmail.com>
2021-01-05 00:01:09 +0000

    Finding tail of an array

---
+++ tail.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  let a = actual;
+  let b = expected;
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${a} === ${b}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${a} !== ${b}`);
+  }
+};
+
+
+const tail = function(array) {
+  return array.slice(1);
+};
+
+
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(words[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(words[1], "Labs"); // ensure second element is "Labs"
