2835e3c20f4808c1b62a3ffcae09a9e3a9e87578
Galit <galit.gerasimov3@gmail.com>
2021-01-06 14:27:44 +0000

    Checking if 2 arrays are perfectly equal

--- eqArrays.js
+++ eqArrays.js
@@ -12,16 +12,35 @@ const assertEqual = function(actual, expected) {
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 
-function eqArrays(array1, array2) {
-  if (array1 === array2) return true;
-  if (array1 == null || array2 == null) return false;
-  if (array1.length !== array2.length) return false;
-
-  for (var i = 0; i < array1.length; ++i) {
-    if (array1[i] !== array2[i]) return false;
+const eqArrays = function(arr1, arr2) {
+ 
+  for (var i = 0; i < arr1.length; i++) {
+    const valueOne = arr1[i]
+    const valueTwo = arr2[i]
+  
+    if (valueOne !== valueTwo) {
+      return false 
+    }
   }
-  return true;
+  return true
+   
+  }
+  
+  assertEqual(eqArrays([1, 3, 2], [1, 2, 3]), true);
+
+//could also do it this way:
+
+// function eqArrays(array1, array2) {
+//   if (array1 === array2) return true;
+//   if (array1 == null || array2 == null) return false;
+//   if (array1.length !== array2.length) return false;
+
+//   for (var i = 0; i < array1.length; ++i) {
+//     if (array1[i] !== array2[i]) return false;
+//   }
+//   return true;
 }
+
 
 // console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
 // console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
@@ -29,4 +48,3 @@ function eqArrays(array1, array2) {
 // console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
 // console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
