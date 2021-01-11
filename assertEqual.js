378ebf82a47580b6f1e955d6ee1970ea8273a02c
Galit <galit.gerasimov3@gmail.com>
2021-01-11 18:43:19 +0000

    Modularizing assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -3,12 +3,12 @@ const assertEqual = function(actual, expected) {
   let a = actual;
   let b = expected;
   if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: " + a + " === " + b);
+    console.log(`✅✅✅ Assertion Passed: ${a} === ${b}`);
   } else {
-    console.log("🛑🛑🛑 Assertion Failed: " + a + " !== " + b);
+    console.log(`🛑🛑🛑 Assertion Failed: ${a} !== ${b}`);
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+
+module.exports = assertEqual;
