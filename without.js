4b00948526f471299f35045eb366c53c7f880d33
Galit <galit.gerasimov3@gmail.com>
2021-01-11 22:57:19 +0000

    Refactored

--- without.js
+++ without.js
@@ -1,28 +1,7 @@
-const assertEqual = function(actual, expected) {
-  let a = actual;
-  let b = expected;
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${a} === ${b}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${a} !== ${b}`);
-  }
-};
+const assertEqual = require('./assertEqual')
+const eqArrays = require('./eqArrays')
+const assertArraysEqual = require('./assertArraysEqual');
 
-function eqArrays(array1, array2) {
-  if (array1 === array2) return true;
-  if (array1 == null || array2 == null) return false;
-  if (array1.length !== array2.length) return false;
-
-  for (var i = 0; i < array1.length; ++i) {
-    if (array1[i] !== array2[i]) return false;
-  }
-  return true;
-}
-
-const assertArraysEqual = function(array1, array2) {
-  if (!eqArrays(array1, array2)) return(console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`));
-  return (console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`));
-}
 
 function without(source, itemsToRemove) {
  var newArr = [];
@@ -33,3 +12,4 @@ function without(source, itemsToRemove) {
  }
  return newArr;
 }
+module.exports = without;
