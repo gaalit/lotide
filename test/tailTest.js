c92dcdc9f266eead75f6232c90c8db64d275f3c7
Galit <galit.gerasimov3@gmail.com>
2021-01-11 22:08:25 +0000

    Added Mocha & Chai to the test file

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,20 @@
+const assert = require('chai').assert;
 const tail = require("../tail");
 
+describe("#tail", () => {
+  const words = ["Yo Yo", "Lighthouse", "Labs"];
+  it("returns 3 for words.length", () => {
+    assert.strictEqual((words.length), 3);
+  });
+  it("returns ['Lighthouse', 'Labs'] for tail(words)  ", () => {
+    assert.deepEqual(tail(words), ['Lighthouse', 'Labs']);
+  });
+});
 
 
 
 // const words = ["Yo Yo", "Lighthouse", "Labs"];
 // tail(words); // no need to capture the return value since we are not checking it
-// assertEqual(words.length, 3); // original array should still have 3 elements!
-// assertEqual(words[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(tail(words.length, 3)); // original array should still have 3 elements!
+// assertEqual(tail(words[0], "Lighthouse")); // ensure first element is "Lighthouse"
 // assertEqual(words[1], "Labs"); // ensure second element is "Labs"
-
-console.log(tail([1,2,3,4])); // [2,3,4]
