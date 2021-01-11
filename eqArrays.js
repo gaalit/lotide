16027dc90dd4b79c8cac8a1e7b93cc62bf518f6c
Galit <galit.gerasimov3@gmail.com>
2021-01-11 19:10:31 +0000

    Modularizing eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,4 @@
-const assertEqual = function(actual, expected) {
-  let a = actual;
-  let b = expected;
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${a} === ${b}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${a} !== ${b}`);
-  }
-};
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arr1, arr2) {
   if(arr1.length !== arr2.length) { // must check first if the arrays are equal in length
@@ -28,25 +16,8 @@ const eqArrays = function(arr1, arr2) {
    
   }
   
-  assertEqual(eqArrays([1, 3, 2], [1, 2, 3]), true);
-
-//could also do it this way:
-
-// function eqArrays(array1, array2) {
-//   if (array1 === array2) return true;
-//   if (array1 == null || array2 == null) return false;
-//   if (array1.length !== array2.length) return false;
-
-//   for (var i = 0; i < array1.length; ++i) {
-//     if (array1[i] !== array2[i]) return false;
-//   }
-//   return true;
-}
+module.exports = eqArrays;
 
 
-// console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-// console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
 
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
 
