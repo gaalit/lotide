d2b71c3a2e830cc41ee728c69b255c4a227d0246
Galit <galit.gerasimov3@gmail.com>
2021-01-11 22:57:10 +0000

    Refactored

--- takeUntil.js
+++ takeUntil.js
@@ -1,7 +1,10 @@
 const takeUntil = function(array, callback) {     
+  
   let newArray = [];  
-  for(let num of array){
-    if(callback(num)) {
+  
+  for(let num of array){ 
+             
+    if(callback(num)) { // plugging num (which is each element as an array) which then replaces the x..
       break;
    } else {
      newArray.push(num);
@@ -10,12 +13,11 @@ const takeUntil = function(array, callback) {
   return newArray;
 }
 
-
-
+module.exports = takeUntil;
 // const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-
+                                     
 // const results1 = takeUntil(data1, x => x < 0);
-// console.log(results1);            //[ 1, 2, 5, 7, 2 ]
+// console.log(results1);            
 
 // const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 // const results2 = takeUntil(data2, x => x === ',');
