49d81aa418297f71ba2522c63f3ec05081e7363c
Galit <galit.gerasimov3@gmail.com>
2021-01-11 22:06:30 +0000

    Added Mocha & Chai to the test file

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,4 +1,23 @@
+const assert = require('chai').assert;
 const eqArrays = require("../eqArrays");
 
-
-console.log(eqArrays([1, 3, 2], [1, 2, 3]));
+describe("#eqArrays", () => {
+  it("returns false for [1, 2, 3] , [1, 3, 4]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 3, 4]), false);
+  });
+  it("returns true for [1, 2, 3] , [1, 2, 3]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it("returns false for [1, 2, 3] , [1, 2, 3, 4]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
+  });
+  it("returns false for [1, 2, 3] , [1, 3, 2]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 3, 2]), false);
+  });
+  it("returns true for ['1', '2', '3'], ['1', '2', '3']", () => {
+    assert.strictEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
+  });
+  it("returns false for ['1', '2', '3'], ['1', '2', 3]", () => {
+    assert.strictEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
+  });
+});
