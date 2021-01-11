cdded1eec51477ba8050b877bc02f959f146f07e
Galit <galit.gerasimov3@gmail.com>
2021-01-11 22:08:05 +0000

    Added Mocha & Chai to the test file

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,11 @@
+const assert = require('chai').assert;
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual"); 
 
-
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2,3])) // => [2, 3]
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns [3,4] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]); 
+  });
+});
