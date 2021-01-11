61b0acf1d2f21cba0785e973cc60d2a649089633
Galit <galit.gerasimov3@gmail.com>
2021-01-11 22:05:46 +0000

    Added Mocha & Chai to the test file

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,2 +1,8 @@
+const assert = require('chai').assert;
 const assertArraysEqual= require("../assertArraysEqual");
 
+describe("#assertArraysEqual", () => {
+  it("returns true for [1, 2, 3] , [1, 2, 3]", () => {
+    assert.deepEqual(["1", "2", "3"], ["1", "2", "3"]);
+  });
+});
